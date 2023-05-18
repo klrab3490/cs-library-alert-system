f018160e73b3148a4579bd3beaf67c99ef0cbdab
Rahul A B <rahulbushi69@gmail.com>
2023-05-18 16:42:48 +0530

    Updates On Admin Pages  + add components- admin-navbar,tables

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,10 +1,11 @@
 import Provider from '@components/Provider';
 import 'styles/styles.modules.css';
-
+import AdminNav from '@components/AdminNav';
 
 const AdminLayout = ({ children }) => {
     return (
         <Provider>
+            <AdminNav />
             <section className='app'>{children}</section>
         </Provider>
     )
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,67 +1,8 @@
-import Link from "next/link";
-import { MdDashboard } from "react-icons/md";
-import { AiOutlineUser } from "react-icons/ai";
-import { TbBooks } from "react-icons/tb";
 import Widget from "@components/widget";
 
 const page = () => {
   return (
     <div className="">
-      <div className="">
-        {/* Nav */}
-          <div className="w-full flex mb-3 pt-3 flex-center">
-            <ul className="hidden sm:flex gap-4">
-                <li className="logo_text hover:text-gray-600">
-                  <Link href="/admin" className="flex">
-                    <MdDashboard className="icon"/>
-                    Dashboard
-                  </Link>
-                </li>
-                <li className="logo_text hover:text-gray-600">
-                  <Link href="/admin/users" className="flex">
-                    <AiOutlineUser />
-                    Users
-                  </Link>
-                </li>
-                <li className="logo_text hover:text-gray-600">
-                  <Link href="/admin/books" className="flex">
-                    <TbBooks />
-                    Books
-                  </Link>
-                </li>
-            </ul>
-          </div>
-          <div className="w-full">
-            <div className="w-full flex mb-3 pt-3 items-center justify-center">
-              <ul className="flex sm:hidden gap-4 font-satoshi font-semibold text-l text-black tracking-wide">
-                <li>
-                  <center>
-                    <Link href='/admin'>
-                      <MdDashboard className="flex-col" />
-                      Dashboard
-                    </Link> 
-                  </center>
-                </li>
-                <li>
-                  <center>
-                    <Link href='/admin/users'>
-                      <AiOutlineUser className="flex-col" />
-                      Users
-                    </Link>
-                  </center>
-                </li>
-                <li>
-                  <center>
-                    <Link href='/admin/books'>
-                      <TbBooks className="flex-col" />
-                      Books
-                    </Link>
-                  </center>
-                </li>
-              </ul>
-            </div>
-          </div>
-      </div>
       {/* Body */}
       <div className="w-full">
         <div className="gap-4 sm:flex">
