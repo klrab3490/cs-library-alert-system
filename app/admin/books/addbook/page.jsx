61a86f6485ffd55da77d626d19366b87b5405d5b
klrab3490 <rahulbushi69@gmail.com>
2023-05-22 22:05:56 +0530

    Updates

---
+++ app/admin/books/addbook/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1> Add New Books </h1>
+
+    </div>
+  )
+}
+
+export default page;
--- app/users/page.jsx
+++ app/users/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1> User Dashboard </h1>
+      <h1 className='font-satoshi font-semibold'> User Dashboard </h1>
     </div>
   )
 }
